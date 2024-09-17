8627ef42345b57d069242bce9b4f4cddfeee0221
Mohammad Ziad Ladadweh <1200418@student.birzeit.edu>
2024-09-17 18:20:26 +0300

    test2

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -21,6 +21,9 @@ import { useTranslation } from "react-i18next";
 import FinancialReport from "./hall_owner/FinancialReport";
 import "./Dashboard.css";
 
+// Import Font Awesome icons
+import { FaUserEdit, FaCalendarAlt, FaPlusSquare, FaHome, FaUsers, FaTrashAlt, FaFileInvoiceDollar, FaCog } from 'react-icons/fa';
+
 const Dashboard = () => {
   const { path, url } = useRouteMatch();
   const { t } = useTranslation();
@@ -43,65 +46,69 @@ const Dashboard = () => {
     <div className="dashboard-container">
       <nav className="dashboard-nav">
         <ul className="links-container">
+          {/* Edit Profile */}
           <li className="nav-item nav-edit-profile">
             <NavLink to={`${url}/edit-profile`} className="nav-link">
-              {t("edit_profile")}
+              <FaUserEdit /> {t("edit_profile")}
             </NavLink>
           </li>
 
+          {/* CUSTOMER Role */}
           {userRole === "CUSTOMER" && (
             <li className="nav-item">
               <NavLink to={`${url}/reserved-halls`} className="nav-link">
-                {t("reserved_halls")}
+                <FaCalendarAlt /> {t("reserved_halls")}
               </NavLink>
             </li>
           )}
 
+          {/* HALL OWNER Role */}
           {userRole === "HALL_OWNER" && (
             <>
               <li className="nav-item">
                 <NavLink to={`${url}/reserved-halls`} className="nav-link">
-                  {t("reserved_halls")}
+                  <FaCalendarAlt /> {t("reserved_halls")}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink to={`${url}/add-new-hall`} className="nav-link">
-                  {t("add_new_hall")}
+                  <FaPlusSquare /> {t("add_new_hall")}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink to={`${url}/my-halls`} className="nav-link">
-                  {t("my_halls")}
+                  <FaHome /> {t("my_halls")}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink to={`${url}/deleted-halls`} className="nav-link">
-                  {t("deleted_halls")}
+                  <FaTrashAlt /> {t("deleted_halls")}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink to={`${url}/financial-report`} className="nav-link">
-                  {t("financial_report")}
+                  <FaFileInvoiceDollar /> {t("financial_report")}
                 </NavLink>
               </li>
             </>
           )}
 
+          {/* ADMIN Role */}
           {userRole === "ADMIN" && (
             <>
               <li className="nav-item">
                 <NavLink to={`${url}/all-users`} className="nav-link">
-                  {t("all_users")}
+                  <FaUsers /> {t("all_users")}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink to={`${url}/procces-halls`} className="nav-link">
-                  {t("Process halls")}
+                  <FaCog /> {t("Process halls")}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink to={`${url}/delete-user`} className="nav-link">
-                  {t("delete_user")}
+                  <FaTrashAlt /> {t("delete_user")}
                 </NavLink>
               </li>
             </>
